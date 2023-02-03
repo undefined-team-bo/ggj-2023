36e38e33c3d2c5af0694cf3ca9337c8d806c7039
Vucket <vucket07@gmail.com>
2023-02-04 00:04:34 +0100

    Feat: Adding player test scene & autospawn

--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -6,6 +6,7 @@ public class PlayerHandler : MonoBehaviour
 {
     public float spawnRadius = 1f;
     public GameObject prefabToSpawn;
+    public bool autoSpawn = false;
     
     [Header("Camera")]
     public CinemachineVirtualCamera gameplayVCam;
@@ -17,6 +18,12 @@ public class PlayerHandler : MonoBehaviour
     private void Awake()
     {
         if (gameplayVCam != null) gameplayVCam.gameObject.SetActive(false);
+    }
+
+    private void Start() {
+        if (autoSpawn) {
+            SpawnPlayer();
+        }
     }
 
     public void SpawnPlayer()
