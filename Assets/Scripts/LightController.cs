bb9544fee4317124b048e733ad807ce06b43153a
Vucket <vucket07@gmail.com>
2023-02-04 20:21:46 +0100

    Feat: Adding light controller

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+}
