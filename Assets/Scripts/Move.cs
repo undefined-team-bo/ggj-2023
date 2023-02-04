f3e17635b65a9c24628bd4fa780a98fa23eca1b5
Vucket <vucket07@gmail.com>
2023-02-04 01:19:35 +0100

    Feat: Adding simple dash to player

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,16 +2,41 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed = 4f;
+    public float dashImpulse = 20f;
 
-    void Update()
-    {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-    
-        var spf = speed * Time.deltaTime;
+    private Rigidbody _rigidBody;
 
-        transform.position += transform.forward * (v * spf);
-        transform.position += transform.right * (h * spf);
+    private float _dashCD = 5f;
+    private float _currentDashCD = 0f;
+    private bool _shouldDash;
+
+    void Start() {
+       _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        // Dash
+        if (_currentDashCD < 0.1 && Input.GetKeyDown(KeyCode.Space)) {
+            _shouldDash = true;
+            _currentDashCD = _dashCD;
+        } else if (_currentDashCD > 0f) {
+            _currentDashCD -= Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate() {
+        float inputHorizontal = Input.GetAxisRaw("Horizontal");
+        float inputVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3 (inputHorizontal, 0.0f, inputVertical);
+
+        _rigidBody.MovePosition(transform.position + movement * Time.deltaTime * speed);
+
+        // Dash RB
+        if (_shouldDash) {
+            _shouldDash = false;
+            _rigidBody.AddForce(transform.forward * dashImpulse, ForceMode.Impulse);
+        }
     }
 }
